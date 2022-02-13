3e5d3817d44b9ca60e016810a9a4733ea202bc6a
Sergio <popckovM10@yandex.ru>
2022-02-14 02:51:07 +0300

    Добавил Vue формы для создания новых VPS через Hasura

--- postgres_hasura/graphql.md
+++ postgres_hasura/graphql.md
@@ -13,10 +13,40 @@
       }
     }
 
+Мутация создания обьекта::
 
+    mutation {
+      insert_api_vps(objects: {cpu: "111", hdd: "222", ram: "333", status: "started", uid: "a3d8ede0-84dd-4a0f-9bda-b799f1c7a7eb"}) {
+        returning {
+          id
+          uid
+          ram
+          cpu
+          hdd
+          status
+          create_date
+          update_date
+        }
+      }
+    }
 
-
+Его JSON версия  
      
+    {"query": "mutation insertMutation ($uid: uuid!, $status: String!, $ram: smallint!, $hdd: smallint!, $cpu: smallint!) {insert_api_vps(objects: {cpu: $cpu, hdd: $hdd, ram: $ram, status: $status, uid: $uid}) { returning { id, uid, ram, cpu, hdd, status, create_date, update_date}}}",
+      "variables": {
+        "ram": 111,
+        "cpu": 111,
+        "hdd": 111,
+        "uid": "23c75906-7e3c-489c-aaeb-ea2cf05b9e2a",
+        "status": "started"
+      }
+    }
+
+
+
+    
+
+
 
 
 
--- vps_front/src/main.js
+++ vps_front/src/main.js
@@ -23,8 +23,10 @@ let api_url = {
 
 // GraphQL запросы к Hasura
 let graphql_query = {
+
   // запрос на получение всех обьектов VPS через GraphQL
   get_all: {query: "{api_vps {id, uid, cpu, hdd, ram, status}}"},
+
   // мутация на изменение статуса состояния одного обьекта VPS
   mutation_change_status: {
     "query": "mutation updateMutation ($uid: uuid!, $status: String!) {update_api_vps(where: {uid: {_eq: $uid}},_set: {status: $status}) {affected_rows returning {status, id, uid}}}",
@@ -32,7 +34,19 @@ let graphql_query = {
       "uid": null,
       "status": null
     }
-  }
+  },
+
+  // мутация на создание нового VPS
+  mutation_crate_new_vps: {
+    "query": "mutation insertMutation ($uid: uuid!, $status: String!, $ram: smallint!, $hdd: smallint!, $cpu: smallint!) {insert_api_vps(objects: {cpu: $cpu, hdd: $hdd, ram: $ram, status: $status, uid: $uid}) { returning { id, uid, ram, cpu, hdd, status, create_date, update_date}}}",
+      "variables": {
+        "ram": null,
+        "cpu": null,
+        "hdd": null,
+        "uid": null,
+        "status": null
+      }
+    }
 }
 
 
@@ -78,37 +92,30 @@ var api1 = new Vue({
 
 
 // Компонент для работы с формой отправки, создание нового VPS через API DRF
-var api1 = new Vue({
+var api_form = new Vue({
   el: '#create-vps-api-form',
-
   data:  {
     cpu: '',
     ram: '',
     hdd: '',
-    current_status: 'started',
-    statuses: [
-      {label: 'started', value: 'started'},
-      {label: 'blocked', value: 'blocked'},
-      {label: 'started', value: 'started'}
-    ]
+    status: 'started',
+    response: null,
+    message: '',
+  },
+  methods: {
+    AddVps: function() {
+      event.preventDefault();
+      axios
+        .post(
+          api_url.api_vps_drf,
+          {cpu: Number(this.cpu), ram: Number(this.ram), hdd: Number(this.hdd), status: this.status }
+        )
+        .then(response => (this.response = response.data), this.message = "Новый VPS добавлен")
+        .catch(error => console.log(error));
+        api1.getAllVps();
+    }
   }
 });
-
-// export default {
-//   data(){
-//     return {
-//       cpu: '',
-//       ram: '',
-//       hdd: '',
-//       current_status: 'started',
-//       statuses: [
-//         {label: 'started', value: 'started'},
-//         {label: 'blocked', value: 'blocked'},
-//         {label: 'started', value: 'started'}
-//       ]
-//     }
-//   }
-// }
 
 
 
@@ -134,7 +141,7 @@ var api2 = new Vue({
         axios
           .post(api_url.api_vps_hasura, graphql_query.get_all)
           .then(response => (this.vps_list = response.data["data"]["api_vps"]))
-          .catch(error => console.log(error));;
+          .catch(error => console.log(error));
         console.info('VPS = ', this.vps_list)
     },
     // Меняет статус обьекта VPS по API
@@ -152,7 +159,45 @@ var api2 = new Vue({
 
 
 
+// Компонент для работы с формой отправки, создание нового VPS через Hasura
+var hasura_form = new Vue({
+  el: '#create-vps-hasura-form',
+  data:  {
+    cpu: '',
+    ram: '',
+    hdd: '',
+    status: 'started',
+    response: null,
+    message: '',
+  },
+  methods: {
+    AddVps: function() {
+      event.preventDefault();
 
+      graphql_query.mutation_crate_new_vps.variables.status = this.status;
+      graphql_query.mutation_crate_new_vps.variables.cpu = Number(this.cpu);
+      graphql_query.mutation_crate_new_vps.variables.ram = Number(this.ram);
+      graphql_query.mutation_crate_new_vps.variables.hdd = Number(this.hdd);
+      graphql_query.mutation_crate_new_vps.variables.uid = this.createUUID();
+
+      console.log('my_query = ', graphql_query.mutation_crate_new_vps);
+
+      axios
+        .post(api_url.api_vps_hasura, graphql_query.mutation_crate_new_vps)
+        .then(response => (this.response = response.data), this.message = "Новый VPS добавлен")
+        .catch(error => console.log(error));
+    },
+
+    // Функция генерации uuid
+    createUUID: function () {
+     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+     });
+    }
+
+  }
+});
 
 
 
